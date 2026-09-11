3a4fae2c9b376dc77d253f3e9b6e5071f5fc915c
Pushkar Hemant Bihani <phbstudy@gmail.com>
2026-09-12 01:23:56 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import { threatDetectionService } from "./src/services/threatDetectionService";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
